114bc4cf45de93e2a03cdf6f2f56e0c380e132bd
ccolosimo <ccolosimo@deadpool.parasoft.com>
2018-09-13 11:29:56 -0700

    Revert "Update the test"
    This reverts commit b67eafb34fbfd1a2b4f0a0a6bffc7c3c5587a9ab.

--- Demo/src/com/experitest/auto/IOSDemoTest.java
+++ Demo/src/com/experitest/auto/IOSDemoTest.java
@@ -2,42 +2,33 @@ package com.experitest.auto;
 
 import java.net.URL;
 
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Optional;
-import org.testng.annotations.Parameters;
-import org.testng.annotations.Test;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 
 import io.appium.java_client.ios.IOSDriver;
 import io.appium.java_client.ios.IOSElement;
-import io.appium.java_client.remote.IOSMobileCapabilityType;
-import io.appium.java_client.remote.MobileCapabilityType;
 
 public class IOSDemoTest extends BaseTest {
 	protected IOSDriver<IOSElement> driver = null;
 
-	@BeforeMethod
-	@Parameters("deviceQuery")
-	public void setUp(@Optional("@os='ios'") String deviceQuery) throws Exception {
-		init(deviceQuery);
+	@Before
+	public void setUp() throws Exception {
+		init("@os='ios'");
 		// Init application / device capabilities
 		//dc.setCapability(MobileCapabilityType.APP, "cloud:com.experitest.ExperiBank");
 		//dc.setCapability(IOSMobileCapabilityType.BUNDLE_ID, "com.experitest.ExperiBank");
-		dc.setCapability(MobileCapabilityType.APP, "cloud:com.experitest.ExperiBank");
-		dc.setCapability(IOSMobileCapabilityType.BUNDLE_ID, "com.experitest.ExperiBank");
 		dc.setCapability("testName", "IOSDemoTest");
 		driver = new IOSDriver<>(new URL(getProperty("url",cloudProperties) + "/wd/hub"), dc);
 	}
 
 	@Test
 	public void test() {
-		driver.findElement(in.Repo.obj("login.username")).sendKeys("company");
-		driver.findElement(in.Repo.obj("login.password")).sendKeys("company");
-		driver.findElement(in.Repo.obj("login.login")).click();
-		driver.findElement(in.Repo.obj("main.logout")).click();
+		// Enter the test code
+
 	}
 
-	@AfterMethod
+	@After
 	public void tearDown() {
 		driver.quit();
 	}
